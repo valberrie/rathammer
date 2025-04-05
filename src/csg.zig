51ca4092f4e953b913f6796a9c4073fc7156df4c
nik <102893782+nmalthouse@users.noreply.github.com>
2025-04-05 16:10:22 -0500

    speed up csg

--- src/csg.zig
+++ src/csg.zig
@@ -17,57 +17,201 @@ pub const MeshBatch = struct {
 };
 pub const MeshMap = std.StringHashMap(MeshBatch);
 
-const EPSILON: f64 = 2E-14;
-pub fn genMesh(sides: []const Side, alloc: std.mem.Allocator, map: *MeshMap) !void {
-    const scale = 0.1;
-    const MAPSIZE = std.math.maxInt(i32);
-    for (sides) |side| {
-        if (std.mem.startsWith(u8, side.material, "TOOLS"))
-            continue;
+pub var gen_time: u64 = 0;
 
-        const plane = Plane.fromTri(side.plane.tri);
+/// This context exists as the csg generation requires lots of shortlived allocations.
+/// Most of the functions will clobber whatever is in their corresponding array list and return the slice.
+pub const Context = struct {
+    const Self = @This();
+    const INIT_CAPACITY = 20;
+    const EPSILON: f64 = 2E-14;
+    const SideClass = enum {
+        back,
+        front,
+        on,
+    };
+    alloc: std.mem.Allocator,
+    base_winding: [4]Vec3,
+    winding_a: std.ArrayList(Vec3),
+    winding_b: std.ArrayList(Vec3),
 
-        var winding = try baseWinding(plane, @floatFromInt(MAPSIZE / 2), alloc);
+    triangulate_index: std.ArrayList(u32),
+    uvs: std.ArrayList(Vec2),
 
-        for (sides) |subside| {
-            const pl2 = Plane.fromTri(subside.plane.tri);
-            if (plane.norm.dot(pl2.norm) > 1 - EPSILON)
+    clip_winding_sides: std.ArrayList(SideClass),
+    clip_winding_dists: std.ArrayList(f64),
+
+    pub fn init(alloc: std.mem.Allocator) !@This() {
+        return .{
+            .alloc = alloc,
+            .base_winding = undefined,
+            .winding_a = try std.ArrayList(Vec3).initCapacity(alloc, INIT_CAPACITY),
+            .winding_b = try std.ArrayList(Vec3).initCapacity(alloc, INIT_CAPACITY),
+
+            .triangulate_index = try std.ArrayList(u32).initCapacity(alloc, INIT_CAPACITY),
+            .uvs = try std.ArrayList(Vec2).initCapacity(alloc, INIT_CAPACITY),
+            .clip_winding_sides = try std.ArrayList(SideClass).initCapacity(alloc, INIT_CAPACITY),
+            .clip_winding_dists = try std.ArrayList(f64).initCapacity(alloc, INIT_CAPACITY),
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.winding_a.deinit();
+        self.winding_b.deinit();
+        self.triangulate_index.deinit();
+        self.uvs.deinit();
+        self.clip_winding_sides.deinit();
+        self.clip_winding_dists.deinit();
+    }
+
+    pub fn genMesh(self: *Self, sides: []const Side, map: *MeshMap) !void {
+        const scale = 0.1;
+        const MAPSIZE = std.math.maxInt(i32);
+        var timer = try std.time.Timer.start();
+        for (sides) |side| {
+            const plane = Plane.fromTri(side.plane.tri);
+
+            self.winding_a.clearRetainingCapacity();
+            var wind_a = &self.winding_a;
+            try wind_a.appendSlice(try self.baseWinding(plane, @floatFromInt(MAPSIZE / 2)));
+
+            var wind_b = &self.winding_b;
+
+            for (sides) |subside| {
+                const pl2 = Plane.fromTri(subside.plane.tri);
+                if (plane.norm.dot(pl2.norm) > 1 - EPSILON)
+                    continue;
+
+                try self.clipWinding(wind_a.*, wind_b, pl2);
+                const temp = wind_a;
+                wind_a = wind_b;
+                wind_b = temp;
+            }
+
+            if (wind_a.items.len < 3)
                 continue;
 
-            const new_winding = try clipWinding(winding, pl2, alloc);
-            winding.deinit();
-            winding = new_winding;
-        }
+            for (wind_a.items) |*item| {
+                item.* = roundVec(item.*);
+            }
+            const ret = map.getPtr(side.material) orelse continue;
 
-        if (winding.items.len < 3)
-            continue;
-
-        for (winding.items) |*item| {
-            item.* = roundVec(item.*);
+            const indexs = try self.triangulate(wind_a.items, @intCast(ret.mesh.vertices.items.len));
+            const uvs = try self.calcUVCoords(wind_a.items, side, @intCast(ret.tex.w), @intCast(ret.tex.h));
+            _ = timer.reset();
+            try ret.mesh.indicies.appendSlice(indexs);
+            try ret.mesh.vertices.ensureUnusedCapacity(wind_a.items.len);
+            for (wind_a.items, 0..) |vert, i| {
+                try ret.mesh.vertices.append(.{
+                    .x = @floatCast(vert.x() * scale),
+                    .y = @floatCast(vert.y() * scale),
+                    .z = @floatCast(vert.z() * scale),
+                    .u = uvs[i].x,
+                    .v = uvs[i].y,
+                    .nx = 0,
+                    .ny = 0,
+                    .nz = 0,
+                    .color = 0xffffffff,
+                });
+            }
+            gen_time += timer.read();
         }
-        const ret = map.getPtr(side.material) orelse continue;
-
-        const indexs = try triangulate(winding.items, alloc, @intCast(ret.mesh.vertices.items.len));
-        const uvs = try calcUVCoords(winding.items, side, @intCast(ret.tex.w), @intCast(ret.tex.h), alloc);
-        defer uvs.deinit();
-        try ret.mesh.indicies.appendSlice(indexs.items);
-        try ret.mesh.vertices.ensureUnusedCapacity(winding.items.len);
-        for (winding.items, 0..) |vert, i| {
-            try ret.mesh.vertices.append(.{
-                .x = @floatCast(vert.x() * scale),
-                .y = @floatCast(vert.y() * scale),
-                .z = @floatCast(vert.z() * scale),
-                .u = uvs.items[i].x,
-                .v = uvs.items[i].y,
-                .nx = 0,
-                .ny = 0,
-                .nz = 0,
-                .color = 0xffffffff,
-            });
-        }
-        indexs.deinit();
     }
-}
+    //Generate indicies into trianglnes that can be drawin with the uknow, opengl draw indexed
+    pub fn triangulate(self: *Self, winding: []const Vec3, offset: u32) ![]const u32 {
+        self.triangulate_index.clearRetainingCapacity();
+        const ret = &self.triangulate_index;
+        if (winding.len < 3) return ret.items;
+
+        for (1..winding.len - 1) |i| {
+            const ii: u32 = @intCast(i);
+            try ret.append(0 + offset);
+            try ret.append(ii + 1 + offset);
+            try ret.append(ii + offset);
+        }
+
+        return ret.items;
+    }
+    pub fn clipWinding(self: *Self, winding_in: std.ArrayList(Vec3), winding_out: *std.ArrayList(Vec3), plane: Plane) !void {
+        self.clip_winding_sides.clearRetainingCapacity();
+        self.clip_winding_dists.clearRetainingCapacity();
+        var sides = &self.clip_winding_sides;
+        var dists = &self.clip_winding_dists;
+        for (winding_in.items) |wind| {
+            const dist = plane.norm.dot(wind) - plane.dist;
+            try dists.append(dist);
+            if (dist > EPSILON) {
+                try sides.append(.front);
+            } else if (dist < -EPSILON) {
+                try sides.append(.back);
+            } else {
+                try sides.append(.on);
+            }
+        }
+        const front = winding_out;
+        front.clearRetainingCapacity();
+        if (winding_in.items.len == 0) return;
+        try sides.append(sides.items[0]);
+        try dists.append(dists.items[0]);
+
+        for (winding_in.items, 0..) |p_cur, i| {
+            if (sides.items[i] == .on) {
+                try front.append(p_cur);
+                continue;
+            }
+            if (sides.items[i] == .front)
+                try front.append(p_cur);
+
+            if (sides.items[i + 1] == .on or sides.items[i] == sides.items[i + 1])
+                continue;
+
+            const p_next = winding_in.items[(i + 1) % winding_in.items.len];
+            const t = dists.items[i] / (dists.items[i] - dists.items[i + 1]);
+
+            const v = p_next.sub(p_cur).scale(t);
+            try front.append(p_cur.add(v));
+        }
+    }
+
+    pub fn calcUVCoords(self: *Self, winding: []const Vec3, side: Side, tex_w: u32, tex_h: u32) ![]const Vec2 {
+        self.uvs.clearRetainingCapacity();
+        const uvs = &self.uvs;
+        var umin: f32 = std.math.floatMax(f32);
+        var vmin: f32 = std.math.floatMax(f32);
+        const tw: f64 = @floatFromInt(tex_w);
+        const th: f64 = @floatFromInt(tex_h);
+        for (winding) |item| {
+            const uv = .{
+                .x = @as(f32, @floatCast(item.dot(side.uaxis.axis) / (tw * side.uaxis.scale) + side.uaxis.translation / tw)),
+                .y = @as(f32, @floatCast(item.dot(side.vaxis.axis) / (th * side.vaxis.scale) + side.vaxis.translation / th)),
+            };
+            try uvs.append(uv);
+            umin = @min(umin, uv.x);
+            vmin = @min(vmin, uv.y);
+        }
+        const uoff = @floor(umin);
+        const voff = @floor(vmin);
+        for (uvs.items) |*uv| {
+            uv.x -= uoff;
+            uv.y -= voff;
+        }
+
+        return uvs.items;
+    }
+
+    pub fn baseWinding(self: *Self, plane: Plane, size: f64) ![]const Vec3 {
+        const global_up = try getGlobalUp(plane.norm);
+        const right = plane.norm.cross(global_up).norm().scale(size / 2);
+
+        const up = plane.norm.cross(right);
+        const offset = plane.norm.scale(plane.dist);
+        self.base_winding[0] = offset.add(right.scale(-1)).add(up);
+        self.base_winding[1] = offset.add(right.scale(-1)).add(up.scale(-1));
+        self.base_winding[2] = offset.add(right).add(up.scale(-1));
+        self.base_winding[3] = offset.add(right).add(up);
+        return &self.base_winding;
+    }
+};
 
 pub fn getGlobalUp(norm: Vec3) !Vec3 {
     var axis: ?usize = null;
@@ -87,112 +231,12 @@ pub fn getGlobalUp(norm: Vec3) !Vec3 {
     return Vec3.new(0, 1, 0);
 }
 
-//Generate indicies into trianglnes that can be drawin with the uknow, opengl draw indexed
-pub fn triangulate(winding: []const Vec3, alloc: std.mem.Allocator, offset: u32) !std.ArrayList(u32) {
-    var ret = std.ArrayList(u32).init(alloc);
-    if (winding.len < 3) return ret;
-
-    for (1..winding.len - 1) |i| {
-        const ii: u32 = @intCast(i);
-        try ret.append(0 + offset);
-        try ret.append(ii + 1 + offset);
-        try ret.append(ii + offset);
-    }
-
-    return ret;
-}
-
 pub fn roundVec(v: Vec3) Vec3 {
     var a = v;
     const R: f64 = 128;
     const rr = @Vector(3, f64){ R, R, R };
     a.data = @round(v.data * rr) / rr;
     return a;
-}
-pub fn clipWinding(winding: std.ArrayList(Vec3), plane: Plane, alloc: std.mem.Allocator) !std.ArrayList(Vec3) {
-    const SideClass = enum {
-        back,
-        front,
-        on,
-    };
-
-    var sides = try std.ArrayList(SideClass).initCapacity(alloc, winding.items.len + 1);
-    defer sides.deinit();
-    var dists = try std.ArrayList(f64).initCapacity(alloc, winding.items.len + 1);
-    defer dists.deinit();
-    for (winding.items) |wind| {
-        const dist = plane.norm.dot(wind) - plane.dist;
-        try dists.append(dist);
-        if (dist > EPSILON) {
-            try sides.append(.front);
-        } else if (dist < -EPSILON) {
-            try sides.append(.back);
-        } else {
-            try sides.append(.on);
-        }
-    }
-    var front = std.ArrayList(Vec3).init(alloc);
-    if (winding.items.len == 0) return front;
-    try sides.append(sides.items[0]);
-    try dists.append(dists.items[0]);
-
-    for (winding.items, 0..) |p_cur, i| {
-        if (sides.items[i] == .on) {
-            try front.append(p_cur);
-            continue;
-        }
-        if (sides.items[i] == .front)
-            try front.append(p_cur);
-
-        if (sides.items[i + 1] == .on or sides.items[i] == sides.items[i + 1])
-            continue;
-
-        const p_next = winding.items[(i + 1) % winding.items.len];
-        const t = dists.items[i] / (dists.items[i] - dists.items[i + 1]);
-
-        const v = p_next.sub(p_cur).scale(t);
-        try front.append(p_cur.add(v));
-    }
-    return front;
-}
-
-pub fn calcUVCoords(winding: []const Vec3, side: Side, tex_w: u32, tex_h: u32, alloc: std.mem.Allocator) !std.ArrayList(Vec2) {
-    var uvs = try std.ArrayList(Vec2).initCapacity(alloc, winding.len);
-    var umin: f32 = std.math.floatMax(f32);
-    var vmin: f32 = std.math.floatMax(f32);
-    const tw: f64 = @floatFromInt(tex_w);
-    const th: f64 = @floatFromInt(tex_h);
-    for (winding) |item| {
-        const uv = .{
-            .x = @as(f32, @floatCast(item.dot(side.uaxis.axis) / (tw * side.uaxis.scale) + side.uaxis.translation / tw)),
-            .y = @as(f32, @floatCast(item.dot(side.vaxis.axis) / (th * side.vaxis.scale) + side.vaxis.translation / th)),
-        };
-        try uvs.append(uv);
-        umin = @min(umin, uv.x);
-        vmin = @min(vmin, uv.y);
-    }
-    const uoff = @floor(umin);
-    const voff = @floor(vmin);
-    for (uvs.items) |*uv| {
-        uv.x -= uoff;
-        uv.y -= voff;
-    }
-
-    return uvs;
-}
-
-pub fn baseWinding(plane: Plane, size: f64, alloc: std.mem.Allocator) !std.ArrayList(Vec3) {
-    var verts = try std.ArrayList(Vec3).initCapacity(alloc, 4);
-    const global_up = try getGlobalUp(plane.norm);
-    const right = plane.norm.cross(global_up).norm().scale(size / 2);
-
-    const up = plane.norm.cross(right);
-    const offset = plane.norm.scale(plane.dist);
-    try verts.append(offset.add(right.scale(-1)).add(up));
-    try verts.append(offset.add(right.scale(-1)).add(up.scale(-1)));
-    try verts.append(offset.add(right).add(up.scale(-1)));
-    try verts.append(offset.add(right).add(up));
-    return verts;
 }
 
 const Plane = struct {
--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,7 @@ const csg = @import("csg.zig");
 const vdf = @import("vdf.zig");
 const vmf = @import("vmf.zig");
 
-fn procSolid(alloc: std.mem.Allocator, solid: vmf.Solid, matmap: *csg.MeshMap, dir: std.fs.Dir) !void {
+fn procSolid(csgctx: *csg.Context, alloc: std.mem.Allocator, solid: vmf.Solid, matmap: *csg.MeshMap, dir: std.fs.Dir) !void {
     var buf: [256]u8 = undefined;
     var fbs = std.io.FixedBufferStream([]u8){ .buffer = &buf, .pos = 0 };
     for (solid.side) |side| {
@@ -20,6 +20,7 @@ fn procSolid(alloc: std.mem.Allocator, solid: vmf.Solid, matmap: *csg.MeshMap, d
             //std.debug.print("{s}\n", .{fbs.getWritten()});
             res.value_ptr.* = .{ .tex = blk: {
                 const bmp = graph.Bitmap.initFromPngFile(alloc, dir, fbs.getWritten()) catch {
+                    std.debug.print("Can't find texture: {s}\n", .{side.material});
                     break :blk graph.Texture.initEmpty();
                 };
 
@@ -29,12 +30,12 @@ fn procSolid(alloc: std.mem.Allocator, solid: vmf.Solid, matmap: *csg.MeshMap, d
             res.value_ptr.mesh = meshutil.Mesh.init(alloc, res.value_ptr.tex.id);
         }
     }
-    try csg.genMesh(solid.side, alloc, matmap);
+    try csgctx.genMesh(solid.side, matmap);
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    //defer _ = gpa.detectLeaks();
+    defer _ = gpa.detectLeaks();
     const alloc = gpa.allocator();
 
     var arg_it = try std.process.argsWithAllocator(alloc);
@@ -45,6 +46,7 @@ pub fn main() !void {
         Arg("vmf", .string, "vmf to load"),
         Arg("scale", .number, "scale the model"),
     }, &arg_it);
+    var draw_tools = true;
 
     //const infile = try std.fs.cwd().openFile("sdk_materials.vmf", .{});
     const infile = try std.fs.cwd().openFile(args.vmf orelse "sdk_materials.vmf", .{});
@@ -65,6 +67,15 @@ pub fn main() !void {
     defer win.destroyWindow();
 
     var matmap = csg.MeshMap.init(alloc);
+    defer {
+        var it = matmap.iterator();
+        while (it.next()) |item| {
+            item.value_ptr.mesh.deinit();
+        }
+        matmap.deinit();
+    }
+    var csgctx = try csg.Context.init(alloc);
+    defer csgctx.deinit();
 
     var draw = graph.ImmediateDrawingContext.init(alloc);
     defer draw.deinit();
@@ -82,24 +93,31 @@ pub fn main() !void {
         .{ .path = "src/basic.frag", .t = .frag },
     });
 
-    const vmf_ = try vdf.fromValue(vmf.Vmf, &.{ .obj = &obj.value }, alloc);
+    var aa = std.heap.ArenaAllocator.init(alloc);
+    defer aa.deinit();
+    const vmf_ = try vdf.fromValue(vmf.Vmf, &.{ .obj = &obj.value }, aa.allocator());
     {
-        var timer = try std.time.Timer.start();
+        var gen_timer = try std.time.Timer.start();
         const dir = try std.fs.cwd().openDir("pngmat", .{});
         for (vmf_.world.solid) |solid| {
-            try procSolid(alloc, solid, &matmap, dir);
+            try procSolid(&csgctx, alloc, solid, &matmap, dir);
             //try meshes.append(try csg.genMesh(solid.side, alloc));
         }
         for (vmf_.entity) |ent| {
             for (ent.solid) |solid|
-                try procSolid(alloc, solid, &matmap, dir);
+                try procSolid(&csgctx, alloc, solid, &matmap, dir);
         }
+        var t2 = try std.time.Timer.start();
         var it = matmap.valueIterator();
+        const nm = matmap.count();
         while (it.next()) |item| {
             item.mesh.setData();
         }
-        const time = timer.read();
-        std.debug.print("csg took {d:.2} ms\n", .{time / std.time.ns_per_ms});
+        const set_time = t2.read();
+        const whole_time = gen_timer.read();
+
+        std.debug.print("csg took {d} {d:.2} us, {d:.2}\n", .{ nm, csg.gen_time / std.time.ns_per_us / nm, set_time / std.time.ns_per_ms });
+        std.debug.print("Generated {d} meshes in {d:.2} ms\n", .{ nm, whole_time / std.time.ns_per_ms });
     }
 
     var cam = graph.Camera3D{};
@@ -110,6 +128,8 @@ pub fn main() !void {
     while (!win.should_exit) {
         try draw.begin(0x75573cff, win.screen_dimensions.toF());
         win.pumpEvents(.poll);
+        if (win.keyRising(.TAB))
+            draw_tools = !draw_tools;
         cam.updateDebugMove(.{
             .down = win.keyHigh(.LSHIFT),
             .up = win.keyHigh(.SPACE),
@@ -125,10 +145,12 @@ pub fn main() !void {
         draw.cube(V3f.new(0, 0, 0), V3f.new(1, 1, 1), 0xffffffff);
         //graph.c.glPolygonMode(graph.c.GL_FRONT_AND_BACK, graph.c.GL_LINE);
         const view_3d = cam.getMatrix(draw.screen_dimensions.x / draw.screen_dimensions.y, 0.1, 100000);
-        var it = matmap.valueIterator();
+        var it = matmap.iterator();
         const mat = graph.za.Mat4.identity().rotate(-90, graph.za.Vec3.new(1, 0, 0));
         while (it.next()) |mesh| {
-            mesh.mesh.drawSimple(view_3d, mat, basic_shader);
+            if (!draw_tools and std.mem.startsWith(u8, mesh.key_ptr.*, "TOOLS"))
+                continue;
+            mesh.value_ptr.mesh.drawSimple(view_3d, mat, basic_shader);
         }
 
         try draw.end(cam);
