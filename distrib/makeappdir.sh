1cb9aaaa0702370201dae86998d655d3f28d5433
nik <102893782+nmalthouse@users.noreply.github.com>
2025-08-04 18:16:54 -0500

    fix

--- distrib/makeappdir.sh
+++ distrib/makeappdir.sh
@@ -27,20 +27,23 @@ cp distrib/appimageskeleton/* "$RATOUT"/
 
 mkdir "$RATOUT"/lib
 LOUT="$RATOUT"/lib
-cp /usr/lib/libfreetype.so.6 $LOUT
+LIBPATH="/lib/x86_64-linux-gnu"
+
+
+cp "$LIBPATH"/libfreetype.so.6 $LOUT
 cp /usr/lib/libSDL3.so.0  $LOUT
-cp /usr/lib/libepoxy.so.0  $LOUT
-cp /usr/lib/libz.so.1  $LOUT
-cp /usr/lib/libm.so.6  $LOUT
+cp "$LIBPATH"/libepoxy.so.0  $LOUT
+cp "$LIBPATH"/libz.so.1  $LOUT
+cp "$LIBPATH"/libm.so.6  $LOUT
 #cp /usr/lib/libc.so.6 $LOUT
 #cp /usr/lib64/ld-linux-x86-64.so.2 $LOUT
-cp /usr/lib/libbz2.so.1.0  $LOUT
-cp /usr/lib/libpng16.so.16  $LOUT
-cp /usr/lib/libharfbuzz.so.0  $LOUT
-cp /usr/lib/libbrotlidec.so.1  $LOUT
+cp "$LIBPATH"/libbz2.so.1.0  $LOUT
+cp "$LIBPATH"/libpng16.so.16  $LOUT
+cp "$LIBPATH"/libharfbuzz.so.0  $LOUT
+cp "$LIBPATH"/libbrotlidec.so.1  $LOUT
 #cp /usr/lib/libglib-2.0.so.0  $LOUT
-cp /usr/lib/libgraphite2.so.3 $LOUT
-cp /usr/lib/libbrotlicommon.so.1 $LOUT
-cp /usr/lib/libpcre2-8.so.0 $LOUT
+cp "$LIBPATH"/libgraphite2.so.3 $LOUT
+cp "$LIBPATH"/libbrotlicommon.so.1 $LOUT
+cp "$LIBPATH"/libpcre2-8.so.0 $LOUT
 
 #cp -r rat_custom "$RATOUT"
