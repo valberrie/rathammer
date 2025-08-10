e27a223e5f408aabc8b795e9f51a43c651605068
nik <102893782+nmalthouse@users.noreply.github.com>
2025-08-10 01:34:21 -0500

    yml no good

---
+++ scripts/build_sdl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone --depth 1 --branch release-3.2.16 https://github.com/libsdl-org/SDL.git
+cd SDL
+sudo apt-get install build-essential git make pkg-config cmake ninja-build gnome-desktop-testing libasound2-dev libpulse-dev libaudio-dev libjack-dev libsndio-dev libx11-dev libxext-dev libxrandr-dev libxcursor-dev libxfixes-dev libxi-dev libxss-dev libxtst-dev libxkbcommon-dev libdrm-dev libgbm-dev libgl1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libdbus-1-dev libibus-1.0-dev libudev-dev
+cmake -S . -B build
+cmake --build build
+sudo cmake --install build --prefix /usr
+cd ..
