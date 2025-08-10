f811a4de4dff48f90fc09a1ca55db399701898a7
nik <102893782+nmalthouse@users.noreply.github.com>
2025-08-10 02:29:24 -0500

    fix the freetype

--- scripts/build_sdl.sh
+++ scripts/build_sdl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone --depth 1 --branch release-3.2.16 https://github.com/libsdl-org/SDL.git
 cd SDL
-sudo apt-get install 
+sudo apt-get install libepoxy-dev libfreetype-dev
 sudo apt-get install build-essential git make pkg-config cmake ninja-build gnome-desktop-testing libasound2-dev libpulse-dev libaudio-dev libjack-dev libsndio-dev libx11-dev libxext-dev libxrandr-dev libxcursor-dev libxfixes-dev libxi-dev libxss-dev libxtst-dev libxkbcommon-dev libdrm-dev libgbm-dev libgl1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libdbus-1-dev libibus-1.0-dev libudev-dev libpipewire-0.3-dev libwayland-dev libdecor-0-dev liburing-dev
 cmake -S . -B build
 cmake --build build
