5d3cce7fa49470e966b723feeae49d4ea3255310
nik <102893782+nmalthouse@users.noreply.github.com>
2025-08-13 19:13:14 -0500

    update ci stuff

--- distrib/makeappdir.sh
+++ distrib/makeappdir.sh
@@ -32,21 +32,21 @@ LOUT="$RATOUT"/lib
 LIBPATH="/lib/x86_64-linux-gnu"
 
 
-cp "$LIBPATH"/libfreetype.so.6 $LOUT
-cp /usr/lib/libSDL3.so.0  $LOUT
-cp "$LIBPATH"/libepoxy.so.0  $LOUT
-cp "$LIBPATH"/libz.so.1  $LOUT
-cp "$LIBPATH"/libm.so.6  $LOUT
-#cp /usr/lib/libc.so.6 $LOUT
-#cp /usr/lib64/ld-linux-x86-64.so.2 $LOUT
-cp "$LIBPATH"/libbz2.so.1.0  $LOUT
-cp "$LIBPATH"/libpng16.so.16  $LOUT
-cp "$LIBPATH"/libharfbuzz.so.0  $LOUT
-cp "$LIBPATH"/libbrotlidec.so.1  $LOUT
-#cp /usr/lib/libglib-2.0.so.0  $LOUT
-cp "$LIBPATH"/libgraphite2.so.3 $LOUT
-cp "$LIBPATH"/libbrotlicommon.so.1 $LOUT
-cp "$LIBPATH"/libpcre2-8.so.0 $LOUT
+#cp "$LIBPATH"/libfreetype.so.6 $LOUT
+#cp /usr/lib/libSDL3.so.0  $LOUT
+#cp "$LIBPATH"/libepoxy.so.0  $LOUT
+#cp "$LIBPATH"/libz.so.1  $LOUT
+#cp "$LIBPATH"/libm.so.6  $LOUT
+##cp /usr/lib/libc.so.6 $LOUT
+##cp /usr/lib64/ld-linux-x86-64.so.2 $LOUT
+#cp "$LIBPATH"/libbz2.so.1.0  $LOUT
+#cp "$LIBPATH"/libpng16.so.16  $LOUT
+#cp "$LIBPATH"/libharfbuzz.so.0  $LOUT
+#cp "$LIBPATH"/libbrotlidec.so.1  $LOUT
+##cp /usr/lib/libglib-2.0.so.0  $LOUT
+#cp "$LIBPATH"/libgraphite2.so.3 $LOUT
+#cp "$LIBPATH"/libbrotlicommon.so.1 $LOUT
+#cp "$LIBPATH"/libpcre2-8.so.0 $LOUT
 
 # Copyright stuff
 cp ratgraph/c_libs/libspng/LICENSE "$RATOUT"/SPNG_LICENSE
